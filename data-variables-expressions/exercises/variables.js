a27db41371e730d0a82536b4a4bdc873f77c7312
BH-98 <bhume98@gmail.com>
2022-11-29 11:43:06 +0000

    completed data-variables-expressions

---
+++ data-variables-expressions/exercises/variables.js
@@ -0,0 +1,28 @@
+let firstName = "Brandon"
+console.log(firstName)
+console.log(typeof(firstName))
+
+let lastName = "Hume"
+console.log(lastName)
+console.log(typeof(lastName))
+
+let age = 24
+console.log(age)
+console.log(typeof(age))
+
+let hasPets = false
+console.log(hasPets)
+console.log(typeof(hasPets))
+
+let email = "bhume98@gmail.com"
+console.log(email)
+console.log(typeof(email))
+
+let message = `"Hello my name is ${firstName} ${lastName} and I'm ${age} years old"`
+console.log(message)
+let initials = firstName.charAt(0) + lastName.charAt(0)
+console.log(initials)
+let fullName = `${firstName} ${lastName}`
+console.log(fullName)
+let petInfo = hasPets ? "I have pets" : "I don't have pets"
+console.log(petInfo)
