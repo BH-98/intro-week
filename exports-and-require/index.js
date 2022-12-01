ff698d8ab0f57f45e493c3b75ce1302ce7b08241
BH-98 <bhume98@gmail.com>
2022-12-01 12:45:15 +0000

    Export and require complete!

--- exports-and-require/index.js
+++ exports-and-require/index.js
@@ -1,8 +1,16 @@
 const somethingFromFile1 = require('./examples/file-1');
+// Vel
 const somethingFromFile2 = require('./examples/file-2');
+// { tutor : 'vel' }
 const somethingFromFile3 = require('./examples/file-3');
+//[ Function: printHello ]
 const somethingFromFile4 = require('./examples/file-4');
+//{add: [Function: add]}
 const somethingFromFile5 = require('./examples/file-5');
+//{printHello: [Function: printHello], add: [Function: add]}
 const somethingFromFile6 = require('./examples/file-6');
-
-//console.log(REPLACE_ME_WITH_A_VARIABLE_FROM_ABOVE);
+//{}
+console.log(somethingFromFile2.tutor);
+somethingFromFile3();
+somethingFromFile5.printHello();
+//All Correct :)
